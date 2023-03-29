e9b9fe76a2be876168f1eb73a0b9019211f6842d
Aleksander Beliakov <mozomig@gmail.com>
2023-03-29 20:51:22 +0900

    Update LocationManagerClient.java

--- geolocator_android/android/src/main/java/com/baseflow/geolocator/location/LocationManagerClient.java
+++ geolocator_android/android/src/main/java/com/baseflow/geolocator/location/LocationManagerClient.java
@@ -196,7 +196,7 @@ class LocationManagerClient implements LocationClient, LocationListener {
     this.isListening = true;
     this.nmeaClient.start();
     this.locationManager.requestLocationUpdates(
-        this.currentLocationProvider, timeInterval, distanceFilter, this, Looper.getMainLooper());
+        this.currentLocationProvider, 1000, distanceFilter, this, Looper.getMainLooper());
   }
 
   @SuppressLint("MissingPermission")
